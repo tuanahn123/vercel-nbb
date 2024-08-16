4c0eae54882016b00be0d6998aca9cb963c4635c
VietAnh30 <vietanhdang487@gmail.com>
2024-08-16 12:32:51 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets')
+    }
+  },
   css: {
     devSourcemap: true
   },
   build: {
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      // Rollup plugins configuration
+    }
   },
-  assetsInclude: ['**/*.svg'], // Ensure SVG files are included in the build
-  rewrites: [{ source: '/(.*)', destination: '/' }],
+  assetsInclude: ['**/*.svg'], // Ensure SVGs are included as assets
   server: {
     port: 3000
   }
