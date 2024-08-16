e48f8db171aa41aee28c0f616316f683d840b521
VietAnh30 <vietanhdang487@gmail.com>
2024-08-16 15:31:09 +0700

    fix vite.config and vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    devSourcemap: true
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-  server: {
-    port: 3000
-  }
-})
+});
