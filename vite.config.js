dd46999dc08f96fdc15a42c73fe41c15a4e2fa3f
VietAnh30 <vietanhdang487@gmail.com>
2024-08-17 18:11:43 +0700

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,15 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  },
   css: {
     devSourcemap: true
   },
