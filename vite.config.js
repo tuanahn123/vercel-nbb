b8525ec3abf00d4ea2ac071fa597f6b26e112163
VietAnh30 <vietanhdang487@gmail.com>
2024-08-17 19:02:08 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: {
       '~': path.resolve(__dirname, 'src')
     },
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   build: {
     target: 'esnext'
